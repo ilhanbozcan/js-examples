20c1c7480d8c1e0c870d27c5583f63dbc5063c8d
ilhanbozcan <ilhanbozcann@gmail.com>
2019-08-25 18:12:39 +0300

    files added

---
+++ messages/script.js
@@ -0,0 +1,24 @@
+const sendBtn = document.querySelector("#sendBtn");
+const messageIn = document.querySelector("#messageIn");
+const messageOut = document.querySelector("#messageOut");
+
+sendBtn.addEventListener("click",sendMsg)
+
+
+function sendMsg(){
+    let content = messageIn.value;
+    //console.log(content);
+    if (content === ""){
+        alert("Please Enter Valid Value")
+        messageOut.innerHTML = "";
+    }
+    else{
+        messageOut.innerHTML = content;
+        
+
+
+    }
+
+    messageIn.value = "";
+    
+}
